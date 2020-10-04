74749607f0c25dc55e82220cdb056805b2f3d937
Heitor Klaus <heitorklaus@hotmail.com>
2020-10-04 19:18:05 -0300

    äsasa

--- src/main/java/com/solead/model/SavePlant.java
+++ src/main/java/com/solead/model/SavePlant.java
@@ -20,12 +20,158 @@ public class SavePlant {
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "id_Sequence")
 	@SequenceGenerator(name = "id_Sequence", sequenceName = "SAVED_SEQ", initialValue = 1, allocationSize = 1)
 	private Long id;
- 	private String dados;
+	private Long id_usuario;
+	private String geracao;
+	private String cpf;
+	private String cep;
+	private String bairro;
+	private String numero;
+	private String area;
+	private String codigo;
+	private String inversor;
+	private String marcaDoModulo;
+	private String numeroDeModulo;
+	private String peso;
+	private String potencia;
+	private String potenciaDoModulo;
+	private String valor;
+	private String potenciaNovo;
+	private String consumoEmReais;
+	private String consumoEmKw;
+	private String cliente;
+	private String endereco;
+ 	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getGeracao() {
+		return geracao;
+	}
+	public void setGeracao(String geracao) {
+		this.geracao = geracao;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getInversor() {
+		return inversor;
+	}
+	public void setInversor(String inversor) {
+		this.inversor = inversor;
+	}
+	public String getMarcaDoModulo() {
+		return marcaDoModulo;
+	}
+	public void setMarcaDoModulo(String marcaDoModulo) {
+		this.marcaDoModulo = marcaDoModulo;
+	}
+	public String getNumeroDeModulo() {
+		return numeroDeModulo;
+	}
+	public void setNumeroDeModulo(String numeroDeModulo) {
+		this.numeroDeModulo = numeroDeModulo;
+	}
+	public String getPeso() {
+		return peso;
+	}
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
+	public String getPotencia() {
+		return potencia;
+	}
+	public void setPotencia(String potencia) {
+		this.potencia = potencia;
+	}
+	public String getPotenciaDoModulo() {
+		return potenciaDoModulo;
+	}
+	public void setPotenciaDoModulo(String potenciaDoModulo) {
+		this.potenciaDoModulo = potenciaDoModulo;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public String getPotenciaNovo() {
+		return potenciaNovo;
+	}
+	public void setPotenciaNovo(String potenciaNovo) {
+		this.potenciaNovo = potenciaNovo;
+	}
+	public String getConsumoEmReais() {
+		return consumoEmReais;
+	}
+	public void setConsumoEmReais(String consumoEmReais) {
+		this.consumoEmReais = consumoEmReais;
+	}
+	public String getConsumoEmKw() {
+		return consumoEmKw;
+	}
+	public void setConsumoEmKw(String consumoEmKw) {
+		this.consumoEmKw = consumoEmKw;
+	}
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	private String dados;
 	public String getDados() {
 		return dados;
 	}
 	public void setDados(String dados) {
 		this.dados = dados;
+	}
+	public Long getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(Long id_usuario) {
+		this.id_usuario = id_usuario;
 	}
 	
 	 
